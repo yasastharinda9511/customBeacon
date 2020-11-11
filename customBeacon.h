aa0f544325acf3fa5a79d678ebbbb6a887f07354
yasastharinda9511 <yasastharinda9511@gmail.com>
2020-11-11 18:19:44 +0000

    Added main structs

--- customBeacon.h
+++ customBeacon.h
@@ -1,12 +1,31 @@
+#define EMERGENCY_VEHICLE_MESSAGE 0x01
+#define LANE_CHANGING_MESSAGE 0x02
+#define BASIC_SAFETY_MESSAGE 0x03
+#define SUDDEN_BRAKE_MESSAGE 0x04
 
-struct MessageType{
+typedef struct MessageType{
+	char priority;
+	uint8_t messageType;
+	uint32_t count;
+} MsgType;
 
 
-};
+typedef struct VehicelInformation{
+
+	uint8_t type;
+	uint8_t vehicle_id[4];
+	uint16_t present;
+	uint32_t acceleration;
+	uint32_t speed;
+	uint32_t pos;
+	uint8_t dir;
+	uint8_t lane;
+}VehicleInfo;
 
 
-struct VehicelInformation{
+typedef struct MainFrame{
 
+	MsgType message_type;
+	VehicleInfo vehicle_info;
 
-
-};
+} Frame;
