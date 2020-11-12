4155f6758af98aa4d58d1fe5afdddefc953bc4dc
KMuthumala <lgkmuthumala@gmail.com>
2020-11-12 08:54:09 +0000

    added set value method for data insertion in to vendor element buffer for vehicle infomation adding

--- customBeacon.cpp
+++ customBeacon.cpp
@@ -3,3 +3,50 @@
 
 
 
+/* Set Vehicle Properties on the Frame */
+
+
+/* 
+ * this method want current frame, value to be set and the position
+ * 
+ * */
+void setFrameValue(MainFrame *frame,uint8_t dataPosition,uint8_t data){
+	
+	frame->frameBufVendorPtr+dataPosition = data;
+	
+}
+
+
+
+
+
+
+
+/* Get Vehicle Properties */
+
+uint8_t getVehicleType(MainFrame *frame){
+	return frame->vehicle_info.type;
+}
+
+float_t getVehicleSpeed(MainFrame *frame){
+	return frame->vehicle_info.speed;
+}
+
+float_t getVehicleAcceleration(MainFrame *frame){
+	return frame->vehicle_info.acceleration;
+}
+
+uint8_t getVehicleDirection(MainFrame *frame){
+	return frame->vehicle_info.dir;
+}
+
+uint8_t getVehicleLane(MainFrame *frame){
+	return frame->vehicle_info.lane;
+}
+
+
+
+
+
+
+
