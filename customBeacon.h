490a2a56f033761a9a83ceadce8ff6c9ca6e4043
yasastharinda9511 <yasastharinda9511@gmail.com>
2020-11-12 16:07:23 +0000

    new struct added

--- customBeacon.h
+++ customBeacon.h
@@ -1,19 +1,24 @@
 
-
 #include <stdint.h>
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <tins/tins.h>
 
 #define EMERGENCY_VEHICLE_MESSAGE 0x01
 #define LANE_CHANGING_MESSAGE 0x02
 #define BASIC_SAFETY_MESSAGE 0x03
 #define SUDDEN_BRAKE_MESSAGE 0x04
 
+#define VEHICLE_TYPE_CAR 0x01
+#define VEHICLE TYPR_VAN 0x02
+#define VEHICLE_TYPE_BUS 0x03
+#define VEHICLE_TYPE_AMBULANCE 0x04
 
 
 
-
+// Data Types
 typedef struct MessageType{
 	char priority;
 	uint8_t messageType;
@@ -38,19 +43,20 @@ typedef struct MainFrame{
 
 	MsgType message_type;
 	VehicleInfo vehicle_info;
-	uint8_t frameBufVendor[50]; // this is a frame buffer for store vehicle data in vendor elements
 
 } Frame;
 
+typedef struct finalPacket{
+	std :: string ssid;
+	std::vector<uint8_t> frameBufVendor;
+} FinalBeacon;
 
-
-
+//
 
 
 /* Set Vehicle Properties on the Frame */
 
 
-void setFrameValue(MainFrame *frame,uint8_t dataPosition,uint8_t data);
 
 
 
@@ -73,6 +79,13 @@ float getVehicleAcceleration(MainFrame *frame);
 uint8_t getVehicleDirection(MainFrame *frame);
 
 uint8_t getVehicleLane(MainFrame *frame);
+
+
+
+// data converting to the finalpacket and put into the Dot11BeaconFrame//
+
+void mainFrameToFinalBeacon(MainFrame *frame ,FinalBeacon *finalBeacon ,Tins:: Dot11Beacon *beacon);
+
 
 
 
